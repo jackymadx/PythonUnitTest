3b4f0eb064a4b2a3293577f7707cd89b964663e5
jackymadx <jacky.chan@madxstudio.com>
2018-11-07 16:27:00 +0800

    Sample of unit test

---
+++ project/test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from my_sum import sum
+
+class TestSum(unittest.TestCase):
+	def test_list_init(self):
+		"""
+		
+		"""
+		data = [1,2,3]
+		result = sum(data)
+		self.assertEqual(result,6)
+	
+if __name__ == '__main__':
+	unittest.main()	
